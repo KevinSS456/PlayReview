913fcd63f9f5d8865907cbe1105987a8bf34cc1c
MuriloBC2 <murilobarrosdocarmo@gmail.com>
2024-05-07 20:05:24 -0300

    tela de inicio 70% feita

--- review_play/src/pages/Home.jsx
+++ review_play/src/pages/Home.jsx
@@ -3,6 +3,9 @@ import axios from 'axios';
 import { getGame } from '../functions/SearchGame';
 import GameCard from '../components/GameCard';
 import { Link } from 'react-router-dom';
+import Clarity from'../assets/clarity.png'
+import Coração from'../assets/coração.png'
+import Ranking from'../assets/ranking.png'
 
 import '../style/home.css'
 
@@ -15,13 +18,13 @@ const Home = () => {
   const numero = parseInt(timestamp/1000)
   const link_screenn = 'https://api.igdb.com/v4/artworks'
   const metodo_recent = `*, hypes,release_dates.date; where hypes > 0 & release_dates.date > ${numero} & version_parent = null; sort hypes desc; limit 20;`
-  const metodo_GTA = `*; where game = 52189;`
-  const metodo_FF= `*; where game = 31551;`
   const metodo_Senua = `*; where game = 127342;`
+  const metodo_FF= `*; where game = 31551;`
+  const metodo_Elden = `*; where game = 240009;`
 
-  const wallpaper_GTA = getGame(metodo_GTA,'',link_screenn)
-  const wallpaper_FF = getGame(metodo_FF,'',link_screenn)
   const wallpaper_Senua = getGame(metodo_Senua,'',link_screenn)
+  const wallpaper_FF = getGame(metodo_FF,'',link_screenn)
+  const wallpaper_Elden = getGame(metodo_Elden,'',link_screenn)
   const recents = getGame(metodo_recent)
   return (
     
@@ -35,18 +38,30 @@ const Home = () => {
               </ol>
             <div class="carousel-inner">
                 <div class="carousel-item active">
+                <div class="carousel-caption d-none d-md-block">
+                        <h5>Senua's Saga: Hellblade II</h5>
+                        <p>Entre e faça sua review</p>
+                    </div>
                 <Link to='/game/52189'>
-                   {wallpaper_GTA.map(scree =>(<img class="w-100 d-inline-block h-75 d-inline-block" src={scree.url.replace('thumb', '1080p')} alt={'Primeiro Slide'} className="bgHome"/>))}
+                   {wallpaper_Senua.map(scree =>(<img class="w-100  img-fluid" src={scree.url.replace('thumb', '1080p')} alt={'Primeiro Slide'} className="bgHome"/>))}
                 </Link>
                 </div>
                  <div class="carousel-item">
+                 <div class="carousel-caption d-none d-md-block">
+                        <h5>Final Fantasy 15</h5>
+                        <p>Entre e faça sua  review</p>
+                    </div>
                  <Link to='/game/31551'>
-                   {wallpaper_FF.map(scree =>(<img class="w-100 d-inline-block" src={scree.url.replace('thumb', '1080p')} alt={'Segundo Slide'} className="bgHome"/>))[0]}
+                   {wallpaper_FF.map(scree =>(<img class="w-100  img-fluid" src={scree.url.replace('thumb', '1080p')} alt={'Segundo Slide'} className="bgHome"/>))[0]}
                    </Link>
                  </div>
                  <div class="carousel-item">
+                     <div class="carousel-caption d-none d-md-block">
+                        <h5>Elden Ring: Shadow of the Erdtree</h5>
+                        <p>Entre e faça a sua review</p>
+                    </div>
                  <Link to='/game/127342'>
-                    {wallpaper_Senua.map(scree =>(<img class="w-100 d-inline-block h-75 d-inline-block" src={scree.url.replace('thumb', '1080p')} alt={'Terceiro Slide'} className="bgHome"/>))}
+                    {wallpaper_Elden.map(scree =>(<img  class="w-100  h-65  img-fluid" src={scree.url.replace('thumb', '1080p')} alt={'Terceiro Slide'} className="bgHome"/>))}
                     </Link>
                  </div>
             </div>
@@ -59,11 +74,31 @@ const Home = () => {
                <span class="sr-only">Próximo</span>
             </a>
           </div>
-      <Link to='/'>
-          <div className="botao" id='saiba'>
-              <p id="botaoSaibaMais">SAIBA MAIS!</p>
+      </div>
+      <div id="home2">
+          <div className="titleHome2">
+              <p className="title">BEM VINDO A gAMErANK</p>
+              <p className="subtitle">Aqui você descobre os melhores jogos do momento com a opinião de outros gamers!</p>
           </div>
-      </Link>
+            <ul id='infoHome'>
+              <li className='liHome'>
+              <img src={Clarity} alt="sim" className='iconHome'/>
+                <p className='liHomeText'>Salve todos os jogos que já jogou em uma lista única!</p>
+              </li>
+              <li className='liHome'>
+              <img src={Coração} alt="sim" className='iconHome'/>
+                <p className='liHomeText'>Escreva e leia reviews sobre os jogos do momento.</p>
+              </li>
+              <li className='liHome'>
+              <img src={Ranking} alt="sim" className='iconHome'/>
+                <p className='liHomeText'>Compartilhe o seu amor pelo jogo favorito com amigos.</p>
+              </li>
+            </ul>
+          <Link to=''>
+          <div className="botao" id='botaoComeça'>
+            <h3 id='textbotao'>COMECE AGORA !</h3>
+          </div>
+          </Link>
       </div>
         {recents.length === 0 && <p>Carregando..</p>}
        
